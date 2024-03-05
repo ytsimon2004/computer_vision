04797a17a0169b5ea89630a4563a4b53a683a365
Yu-Ting Wei <ytsimon2004@gmail.com>
2024-03-05 10:30:36 +0100

    refactor

--- src/comvis/utils/util_proc_dict.py
+++ src/comvis/utils/util_proc_dict.py
@@ -1,9 +1,9 @@
 import json
+from pathlib import Path
 from typing import TypedDict
 
 import numpy as np
 
-from comvis.utils.util_json import load_from_json, JsonEncodeHandler
 from comvis.utils.util_typing import PathLike
 
 __all__ = [
@@ -51,10 +51,26 @@ DEFAULT_PROC_PARS: ProcessParameters = {
 # JSON IO #
 # ======= #
 
+class JsonEncodeHandler(json.JSONEncoder):
+    """extend from the JSONEncoder class and handle the conversions in a default method"""
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        if isinstance(obj, Path):
+            return str(obj)
+
+        return json.JSONEncoder.default(self, obj)
+
+
 def create_default_json(output_path: PathLike) -> None:
     with open(output_path, "w") as outfile:
         json.dump(DEFAULT_PROC_PARS, outfile, sort_keys=True, indent=4, cls=JsonEncodeHandler)
 
 
-def load_process_parameter(f: PathLike) -> 'ProcessParameters':
-    return load_from_json(f)
+def load_process_parameter(f: PathLike) -> ProcessParameters:
+    with open(f, "r") as file:
+        return json.load(file)
