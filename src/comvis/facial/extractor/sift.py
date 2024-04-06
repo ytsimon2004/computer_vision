5a6c2d069eb964f54ff129dd7cddf6fbb9a5c538
Yu-Ting Wei <ytsimon2004@gmail.com>
2024-04-07 00:21:25 +0200

    kmeans for sift

--- src/comvis/facial/extractor/sift.py
+++ src/comvis/facial/extractor/sift.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 from skimage.color import rgb2gray
+from sklearn.cluster import KMeans
 from typing_extensions import Self
 
 from comvis.facial.extractor.base import IdentityFeatureExtractor, ExtractedResultLike
@@ -46,7 +47,24 @@ class SIFTFeatureExtractor(IdentityFeatureExtractor):
     def __call__(self, X) -> list[SIFTExtractorResult]:
         return self.transform(X)
 
+    def _create_kmeans(self, X, n: int = 50) -> KMeans:
+        features = []
+        for image in X:
+            # ensure image is in grayscale
+            if len(image.shape) == 3:
+                image = normalize_to_sift(image)
+
+            _, des = self.sift.detectAndCompute(image, None)
+            if des is not None:
+                print(f'{des[:self.n_features, :].shape=}')
+                features.append(des[:self.n_features, :])
+
+        return kmean_cluster_descriptor(np.vstack(features), n)
+
     def transform(self, X) -> list[SIFTExtractorResult]:
+        """ """
+        kmeans = self._create_kmeans(X)
+
         features = []
         for image in X:
             # ensure image is in grayscale
@@ -54,7 +72,17 @@ class SIFTFeatureExtractor(IdentityFeatureExtractor):
                 image = normalize_to_sift(image)
 
             keypoints, descriptors = self.sift.detectAndCompute(image, None)
-            features.append((descriptors, keypoints))
+
+            if descriptors is not None:
+                histogram = np.zeros(len(kmeans.cluster_centers_))
+                clusters = kmeans.predict(descriptors)
+
+                for i in clusters:
+                    histogram[i] += 1
+
+                features.append([histogram, keypoints])
+            else:
+                features.append([np.zeros(len(kmeans.cluster_centers_)), keypoints])
 
         return [
             SIFTExtractorResult(img, features[i][0], features[i][1])
@@ -76,6 +104,12 @@ def normalize_to_sift(img: np.ndarray) -> np.ndarray:
         ret = img.astype(np.uint8)  # Or, np.zeros(img.shape, dtype=np.uint8) for a black image
 
     return ret
+
+
+def kmean_cluster_descriptor(descriptors: np.ndarray, n_clusters=50) -> KMeans:
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+    kmeans.fit(descriptors)
+    return kmeans
 
 
 def plot_sift_extracted_result(results: list[SIFTExtractorResult]):
